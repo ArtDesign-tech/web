a95c295c1ea906c38fb052e13841a91842f3828f
ArtDesign-tech <iqbaltodesign@gmail.com>
2025-06-01 08:39:58 +0700

    Update and rename index_simple.php to index.php

--- index.php
+++ index.php
@@ -366,7 +366,7 @@ $item_per_kantin_display = 4; // Jumlah item per kantin untuk display
 </footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="script_simple.js"></script>
+<script src="script.js"></script>
 </body>
 </html>
 <?php
